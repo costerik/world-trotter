9b6e817bb615327219796ef4b53893d1f25ff79d
Erik Ahumada <costerik@hotmail.com>
2021-04-25 16:26:59 -0500

    first commit

---
+++ world-trotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  world-trotter
+//
+//  Created by Erik Alonso Ahumada Sarmiento on 19/04/21.
+//  Copyright © 2021 eaas corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ world-trotterTests/world_trotterTests.swift
@@ -0,0 +1,34 @@
+//
+//  world_trotterTests.swift
+//  world-trotterTests
+//
+//  Created by Erik Alonso Ahumada Sarmiento on 19/04/21.
+//  Copyright © 2021 eaas corp. All rights reserved.
+//
+
+import XCTest
+@testable import world_trotter
+
+class world_trotterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
